a5b65b832ba96456788adf2f111c6bfd28837004
hadi syaputra <hadisyaputra.sumsel@gmail.com>
2026-06-13 10:15:24 +0700

    Add debug echo logs

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -1,11 +1,14 @@
 #!/bin/bash
+echo "Starting entrypoint..."
 
 # Jalankan package discover terlebih dahulu
+echo "Menjalankan package:discover..."
 php artisan package:discover --ansi
 
 # Tunggu database mysql siap
 echo "Menunggu MySQL..."
 while ! php artisan db:show > /dev/null 2>&1; do
+  echo "Menunggu koneksi database..."
   sleep 2
 done
 
